824b30f789a5b42a4bed7da0d75b5badf28f1b31
ElyseD7 <elysed7@nycstudents.net>
2026-09-23 06:25:37 -0700

    ad

---
+++ Data.py
@@ -0,0 +1,24 @@
+# x=3 
+# y=float(3)
+# print(x,y)
+
+# values = [1,2,3,4,.25,30,51]
+# print(values)
+
+# for i in values:
+#     print(i)
+
+# print(values[-1])
+# print(values[1])
+
+# x= "This is a good string"
+# y= x.split()
+# z= y[0]
+# print(y)
+# print(x)
+
+sentence = input("Ask me a question")
+
+y  = sentence.split()
+for i in y:
+    print(i)
